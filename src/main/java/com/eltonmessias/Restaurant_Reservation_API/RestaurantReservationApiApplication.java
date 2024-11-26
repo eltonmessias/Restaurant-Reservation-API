8109193da95a9a7ba034fb59ddfbd9d923cd2f59
elton <eltonguambe1006@gmail.com>
2024-11-26 15:44:15 +0200

    first configurations

---
+++ src/main/java/com/eltonmessias/Restaurant_Reservation_API/RestaurantReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.eltonmessias.Restaurant_Reservation_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReservationApiApplication.class, args);
+	}
+
+}
